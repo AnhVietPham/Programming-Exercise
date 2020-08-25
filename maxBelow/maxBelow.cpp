e02657f77892f74bb9123834fecb547f256487b6
PHAM ANH VIET <pham.anh.viet.vlth@gmail.com>
2020-08-25 14:41:06 +0700

    Good Soluton find maxBelow

--- maxBelow/maxBelow.cpp
+++ maxBelow/maxBelow.cpp
@@ -3,17 +3,12 @@
 
 using namespace std;
 
-long maxLong = LONG_MAX;
-
-int maxBelowBadSolution(int a[], int size, int k)
-{
-       
-}
+long maxInt = INT_MAX;
 
 int maxBelowBetterSolution(int a[], int size, int k)
 {
-       long minDistance = maxLong;
-       long maxBelow = 0;
+       int minDistance = maxInt;
+       int maxBelow = 0;
        for (int i = 0; i < size; i++)
        {
               if (a[i] != a[k] && (a[k] - a[i] > 0) && (a[k] - a[i] < minDistance))
@@ -25,11 +20,25 @@ int maxBelowBetterSolution(int a[], int size, int k)
        return maxBelow;
 }
 
+int maxBelowBetter2Solution(int a[], int size, int k){
+      
+       int maxBelow = 0;
+
+       for (int i = 0; i < size; i++)
+       {
+              if (a[i] < a[k]) {
+                  maxBelow = maxBelow > a[i] ? maxBelow : a[i];
+              }
+       }
+
+       return maxBelow;
+}
+
 int main()
 {
-       int arr[] = {3, 6, 5, 4, 8, 11};
+       int arr[] = {5, 3, 6, 4, 8, 11};
        int arraySize = sizeof(arr) / sizeof(arr[0]);
-       long maxBelow = maxBelowBadSolution(arr, arraySize, 2);
+       long maxBelow = maxBelowBetterSolution(arr, arraySize, 5);
        cout << "Value of max below is " << maxBelow;
        return 0;
 }
