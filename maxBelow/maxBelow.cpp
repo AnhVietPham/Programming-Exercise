d9a82c42741f0133f8f35a6f93df4cf8e6aba931
PHAM ANH VIET <pham.anh.viet.vlth@gmail.com>
2020-08-25 16:57:44 +0700

    reduce condition of if ignore a[k] != a[i]

--- maxBelow/maxBelow.cpp
+++ maxBelow/maxBelow.cpp
@@ -11,7 +11,7 @@ int maxBelowBetterSolution(int a[], int size, int k)
        int maxBelow = 0;
        for (int i = 0; i < size; i++)
        {
-              if (a[i] != a[k] && (a[k] - a[i] > 0) && (a[k] - a[i] < minDistance))
+              if (a[k] - a[i] > 0 && (a[k] - a[i] < minDistance))
               {
                      minDistance = a[k] - a[i];
                      maxBelow = a[i];
@@ -40,8 +40,8 @@ int main()
 {
        int arr[] = {5, 3, 6, 4, 8, 11};
        int arraySize = sizeof(arr) / sizeof(arr[0]);
-       int maxBelow = maxBelowBetterSolution(arr, arraySize, 0);
-       int maxBelowTwo = maxBelowBetter2Solution(arr, arraySize, 0);
+       int maxBelow = maxBelowBetterSolution(arr, arraySize, 5);
+       int maxBelowTwo = maxBelowBetter2Solution(arr, arraySize, 5);
        cout << "Value of max below is " << maxBelow;
        cout << "Value of max below 2 is " << maxBelowTwo;
        return 0;
