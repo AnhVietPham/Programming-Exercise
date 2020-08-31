bfabdf76075782024835bdab005f7f2790bbd67c
PHAM ANH VIET <pham.anh.viet.vlth@gmail.com>
2020-08-31 13:32:41 +0700

    improve code find LongestHill

--- longestHill/longest-increasing-decreasing-subarray.cpp
+++ longestHill/longest-increasing-decreasing-subarray.cpp
@@ -1,24 +1,65 @@
 #include <iostream>
-#include <vector> 
 using namespace std;
 
-int longestInCreasingDecreasingSubArray(int a[], int n)
+void longestInCreasingDecreasingSubArray(int a[], int n)
 {
        int max = 1;
        int lenOfSubArray = 1;
+       int lenOfIncreasingSubArray = 1;
+       int lenOfDecreasingSubArray = 1;
+       int lenOfTheSameValues = 1;
 
-       for (int i = 1; i < n; i++)
+       if (n - 1 <= 2)
        {
-              if (a[i] > a[i - 1])
+              cout << "Size of Array is not enough to create Hill";
+              return;
+       }
+
+       for (int i = 1; i < n - 1; i++)
+       {
+              if (a[i - 1] < a[i])
+              {
+                     lenOfIncreasingSubArray++;
+              }
+
+              if (a[i] < a[i - 1])
+              {
+                     lenOfDecreasingSubArray++;
+              }
+
+              if (a[i] == a[i - 1])
+              {
+                     lenOfTheSameValues++;
+              }
+       }
+
+       if (lenOfIncreasingSubArray == n - 1)
+       {
+              cout << "Value is increasing";
+              return;
+       }
+
+       if (lenOfDecreasingSubArray == n - 1)
+       {
+              cout << "Value is decreasing";
+              return;
+       }
+
+       if (lenOfTheSameValues == n - 1)
+       {
+              cout << "Array has the same values";
+              return;
+       }
+
+       for (int i = 1; i < n - 1; i++)
+       {
+              if (a[i] >= a[i - 1])
               {
                      lenOfSubArray++;
               }
               else
               {
-                     if (lenOfSubArray > 1)
-                     {
-                            lenOfSubArray++;
-                     }
+                     lenOfSubArray++;
                      if (a[i + 1] > a[i])
                      {
                             if (max < lenOfSubArray)
@@ -29,20 +70,30 @@ int longestInCreasingDecreasingSubArray(int a[], int n)
                      }
               }
        }
-
        if (max < lenOfSubArray)
        {
               max = lenOfSubArray;
        }
-       
 
-       return max;
+       cout << "Value of LongestHill is: " << max;
 }
 
 int main()
 {
-       int arr[] = {7, 6, 5, 4, 3, 2, 9, 11, 12, 10, 8, 0};
+       // Case 1: Increasing - decreasing Array
+       // int arr[] = {7, 6, 5, 4, 3, 2, 9, 11, 12, 10, 8, 6, 20, 0};
+       // Case 2: Increasing Array
+       // int arr[] = {2, 3, 4, 7, 9, 11, 0};
+       // Case 3: Decreasing Array
+       // int arr[] = {9, 8, 6, 4, 3, 2, 1, 0};
+       // Case 4: Array is not enough to create a Hill.
+       // int arr[] = {2, 3, 1, 0};
+       // Case 5: 
+       int arr[] = {7, 6, 5, 4, 3, 2, 9, 11, 11, 11, 12, 10, 8, 6, 7, 0};
+       // Case 6: The same values
+       // int arr[] = {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
+       // find solution remove last value == 0;
        int arrSize = sizeof(arr) / sizeof(arr[0]);
-       cout << "Value of LongestHill is: " << longestInCreasingDecreasingSubArray(arr, arrSize);
+       longestInCreasingDecreasingSubArray(arr, arrSize);
        return 0;
 }
