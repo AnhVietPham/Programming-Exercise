c602fea6baa13ee276a29969933c591a1dc74d18
PHAM ANH VIET <pham.anh.viet.vlth@gmail.com>
2020-09-03 22:44:30 +0700

    Homework recursion 730: S(n) = 1 + 2 + 3 ...

---
+++ recursion/homework_730.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int recursion_sum(int n)
+{
+       if (n == 0)
+       {
+              return 0;
+       }
+
+       return n + recursion_sum(n - 1);
+}
+
+int tail_recursion_sum(int n, int sum = 0)
+{
+       if (n == 0)
+       {
+              return n + sum;
+       }
+
+       return tail_recursion_sum(n - 1, n + sum);
+}
+
+int naive_code_sum(int n)
+{
+       int sum = 0;
+
+       for (int i = 1; i <= n; i++)
+       {
+              sum += i;
+       }
+
+       return sum;
+}
+
+int main()
+{
+       int n = 5;
+       cout << "Recursion S(n) is " << recursion_sum(n) << "\n";
+       cout << "Recursion S(n) is " << tail_recursion_sum(n) << "\n";
+       cout << "Recursion S(n) is " << naive_code_sum(n) << "\n";
+       return 0;
+}
