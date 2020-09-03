26b18e8e9b33b8e57f3510fd4df34f43f7bb7aa9
PHAM ANH VIET <pham.anh.viet.vlth@gmail.com>
2020-09-03 13:18:33 +0700

    refactore code calculate_X_n

---
+++ recursion/calculate_X_n.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int calculate_X_n(int n)
+{
+       int sum = 0;
+       if (n == 0)
+       {
+              return 1;
+       }
+
+       for (int i = 1; i <= n; i++)
+       {
+              sum += i * i * calculate_X_n(n - i);
+       }
+
+       return sum;
+}
+
+
+int main()
+{
+       int n = 3;
+       cout << "Sum of X_n is " << calculate_X_n(n);
+       return 0;
+}
