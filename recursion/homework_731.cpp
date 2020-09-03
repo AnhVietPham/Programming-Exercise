5691e7bb3e2aac414935bee058b8cdc1866fc94d
PHAM ANH VIET <pham.anh.viet.vlth@gmail.com>
2020-09-03 22:54:04 +0700

    Homework 731: S(n) = 1^2 + 2^2 + 3^2 + ... + n^2

---
+++ recursion/homework_731.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+// S(n) = 1^2 + 2^2 + 3^2 + ... + n^2
+
+int recursion_731(int n)
+{
+       if (n == 0)
+       {
+              return 0;
+       }
+
+       return recursion_731(n - 1) + n * n;
+}
+
+int tail_recursion_731(int n, int sum = 0)
+{
+       if (n == 0)
+       {
+              return n * n + sum;
+       }
+
+       return tail_recursion_731(n - 1, n * n + sum);
+}
+
+int naive_code_731(int n)
+{
+       int sum = 0;
+
+       for (int i = 1; i <= n; i++)
+       {
+              sum += i * i;
+       }
+
+       return sum;
+}
+
+int main()
+{
+       int n = 3;
+       cout << "Recursion S(n) is " << recursion_731(n) << "\n";
+       cout << "Recursion S(n) is " << tail_recursion_731(n) << "\n";
+       cout << "Recursion S(n) is " << naive_code_731(n) << "\n";
+
+       return 0;
+}
