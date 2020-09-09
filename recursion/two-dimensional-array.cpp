59573dec6c6aab52e81b146d94b9ed78f651ba8f
PHAM ANH VIET <pham.anh.viet.vlth@gmail.com>
2020-09-09 12:57:32 +0700

    Recursion: min and max in 2D-Array

--- recursion/two-dimensional-array.cpp
+++ recursion/two-dimensional-array.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits.h>
 using namespace std;
 
 /*
@@ -38,47 +39,60 @@ void output_2D_array(int **a, int row, int column, int i = 0)
        output_2D_array(a, row, column, i + 1);
 }
 
-int find_min(int **a, int row, int column, int i = 0){
+int find_min(int **a, int row, int column, int i = 0, int MIN = INT_MAX)
+{
 
+       if (i == row * column)
+       {
+              return MIN;
+       }
+
+       return find_min(a, row, column, i + 1, a[i / column][i % column] < MIN ? a[i / column][i % column] : MIN);
 }
 
-int find_max(int **a, int row, int column, int i = 0){
-
+int find_max(int **a, int row, int column, int i = 0, int MAX = 0)
+{
+       if (i == column * row)
+       {
+              return MAX;
+       }
+       return find_max(a, row, column, i + 1, a[i / column][i % column] > MAX ? a[i / column][i % column] : MAX);
 }
 
-int sum_2D_array(int **a, int row, int column, int i = 0, int sum = 0){
+// int sum_2D_array(int **a, int row, int column, int i = 0, int sum = 0)
+// {
+// }
 
-}
+// int quantity_of_even_number_2D_array(int **a, int row, int column, int i = 0, int count = 0)
+// {
+// }
 
-int quantity_of_even_number_2D_array(int **a, int row,int column, int i =0, int count = 0){
+// int quantity_of_odd_number_2D_array(int **a, int row, int column, int i = 0, int cout = 0)
+// {
+// }
 
-}
+// int sum_of_even_numer_2D_array(int **a, int row, int column, int i = 0, int cout = 0)
+// {
+// }
 
-int quantity_of_odd_number_2D_array(int **a, int row, int column, int i = 0, int cout = 0){
+// int sum_of_odd_numer_2D_array(int **a, int row, int column, int i = 0, int cout = 0)
+// {
+// }
 
-}
+// void swap(int &a, int &b)
+// {
+//        int temp = a;
+//        b = a;
+//        a = temp;
+// }
 
-int sum_of_even_numer_2D_array(int **a, int row, int column, int i = 0, int cout = 0){
+// void arrange_increasing_2D_array(int **a, int row, int column, int i = 0)
+// {
+// }
 
-}
-
-int sum_of_odd_numer_2D_array(int **a, int row, int column, int i = 0, int cout = 0){
-
-}
-
-void swap(int &a, int &b){
-       int temp = a;
-       b = a;
-       a = temp;
-}
-
-void arrange_increasing_2D_array(int **a, int row, int column, int i = 0){
-
-}
-
-void arrange_decreasing_2D_array(int **a, int row, int column, int i = 0){
-       
-}
+// void arrange_decreasing_2D_array(int **a, int row, int column, int i = 0)
+// {
+// }
 
 int main()
 {
@@ -94,6 +108,10 @@ int main()
 
        input_2D_array(a, row, column);
        output_2D_array(a, row, column);
+
+       cout << "\nMax of 2D-Array is: " << find_max(a, row, column);
+       cout << "\nMin of 2D-Array is: " << find_min(a, row, column);
+
 
        return 0;
 }
