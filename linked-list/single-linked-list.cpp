d60432ef3f1d045abe850f5ff18e89946cc68906
PHAM ANH VIET <pham.anh.viet.vlth@gmail.com>
2020-09-12 06:40:28 +0700

    Singled Linked List addHead()

--- linked-list/single-linked-list.cpp
+++ linked-list/single-linked-list.cpp
@@ -2,12 +2,12 @@
 
 using namespace std;
 
-struct  Node
+struct Node
 {
        int data;
-       struct  Node *pNext;
+       struct Node *pNext;
 };
-typedef struct  Node NODE;
+typedef struct Node NODE;
 
 struct List
 {
@@ -16,11 +16,13 @@ struct List
 
 typedef struct List LIST;
 
-void init(LIST &list){
+void init(LIST &list)
+{
        list.pHead = list.pTail = NULL;
 }
 
-NODE* createNote(int value){
+NODE *createNote(int value)
+{
        NODE *p = new NODE;
 
        if (p == NULL)
@@ -29,16 +31,25 @@ NODE* createNote(int value){
               return NULL;
        }
 
-       p ->data = value;
-       p ->pNext = NULL;
+       p->data = value;
+       p->pNext = NULL;
        return p;
 }
 
+void addHead(LIST &list, NODE *p)
+{
+       if (list.pHead == NULL)
+       {
+              list.pHead = list.pTail = p;
+       }
+       else
+       {
+              p->pNext = list.pHead;
+              list.pHead = p;
+       }
+}
 
-
-
-
-
-int main(){
+int main()
+{
        return 0;
 }
