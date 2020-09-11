2df7f431c12ed1e8a51f6404c92f7d2d15d5c6ae
PHAM ANH VIET <pham.anh.viet.vlth@gmail.com>
2020-09-12 06:23:46 +0700

    Step 1: declare data structure signle linked list

---
+++ linked-list/single-linked-list.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+struct  Node
+{
+       int data;
+       struct  Node *pNext;
+};
+typedef struct  Node NODE;
+
+struct List
+{
+       NODE *pHead, *pTail;
+};
+
+typedef struct List LIST;
+
+
+
+
+
+
+int main(){
+       return 0;
+}
