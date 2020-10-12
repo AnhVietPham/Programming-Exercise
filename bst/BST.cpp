b9990d1356c0cd50fc6f5f6fe84e0cbfbdd67802
PHAM ANH VIET <pham.anh.viet.vlth@gmail.com>
2020-10-12 22:47:40 +0700

    BST: ridOfRecursionAddNodeIntoThree

--- bst/BST.cpp
+++ bst/BST.cpp
@@ -45,12 +45,57 @@ void addNodeIntoThree(NODE *&root, int data)
        }
 }
 
+void ridOfRecursionAddNodeIntoThree(NODE *&root, int data)
+{
+       if (root == NULL)
+       {
+              root = createNode(data);
+       }
+       else
+       {
+              NODE *p;
+              NODE *temp = root;
+              while (temp != NULL)
+              {
+                     p = temp;
+                     if (data > temp->data)
+                     {
+                            temp = temp->right;
+                     }
+                     else if (data < temp->data)
+                     {
+                            temp = temp->left;
+                     }
+                     else
+                     {
+                            return;
+                     }
+              }
+              if (data > p->data)
+              {
+                     p->right = createNode(data);
+              }
+              else if (data < p->data)
+              {
+                     p->left = createNode(data);
+              }
+       }
+}
+
 void createBST(NODE *&root, int a[], int n)
 {
        init(root);
        for (int i = 0; i < n; i++)
        {
               addNodeIntoThree(root, a[i]);
+       }
+}
+
+void createBSTRidOfRecursion(NODE *&root, int a[], int n){
+       init(root);
+       for (int i = 0; i < n; i++)
+       {
+              ridOfRecursionAddNodeIntoThree(root, a[i]);
        }
 }
 
@@ -74,7 +119,8 @@ void LNR(NODE *root)
        }
 }
 
-void RNL(NODE *root){
+void RNL(NODE *root)
+{
        if (root != NULL)
        {
               RNL(root->right);
@@ -83,20 +129,44 @@ void RNL(NODE *root){
        }
 }
 
+void LRN(NODE *root)
+{
+       if (root != NULL)
+       {
+              LRN(root->left);
+              LRN(root->right);
+              cout << root->data << "   ";
+       }
+}
+
 int main()
 {
        NODE *ROOT;
+       NODE *ROOT1;
        int a[] = {40, 5, 35, 45, 15, 56, 48, 13, 16, 49, 47};
        int n = sizeof(a) / sizeof(a[0]);
 
        createBST(ROOT, a, n);
-       cout << "NLR: "<< "\n";
-       NLR(ROOT);
-       cout<<"\n";
-       cout << "LNR: "<< "\n";
+       createBSTRidOfRecursion(ROOT1, a, n);
+       // cout << "NLR: "
+       //      << "\n";
+       // NLR(ROOT);
+       cout << "\n";
+       cout << "LNR Recursion: "
+            << "\n";
        LNR(ROOT);
-       cout<<"\n";
-       cout << "RNL: "<< "\n";
-       RNL(ROOT);
+
+       cout << "\n";
+       cout << "LNR Rid of Recursion: "
+            << "\n";
+       LNR(ROOT1);
+       // cout << "\n";
+       // cout << "RNL: "
+       //      << "\n";
+       // RNL(ROOT);
+       // cout << "\n";
+       // cout << "LRN: "
+       //      << "\n";
+       // LRN(ROOT);
        return 0;
 }
